9142b15ec5146e4813f6ec1f4773029002177f39
Fra <sworld.design@gmail.com>
2018-05-16 13:25:10 +0200

    fix db

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article
 # Create your views here.
 def articles(request):
     # return HttpResponse('Hello from Python!')
-    return render(request, 'articles/list.html')
+    articles = Article.objects.all().order_by('date')
+    return render(request, 'articles/list.html', {'articles':articles})
