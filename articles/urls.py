2c36a14fe606a9d9513e587d2ec033284dc1db2f
Fra <sworld.design@gmail.com>
2018-05-19 12:57:24 +0200

    article detail

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,9 @@
-from . import views
 from django.conf.urls import url
+from . import views
+from django.urls import path
+app_name = 'articles'
 
 urlpatterns = [
-    url(r'^', views.articles),
+    url(r'^$', views.article_list, name="list"),
+    path("<slug:slug>/", views.article_detail, name="detail"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Article
 # Create your views here.
-def articles(request):
+def article_list(request):
     # return HttpResponse('Hello from Python!')
     articles = Article.objects.all().order_by('date')
     return render(request, 'articles/list.html', {'articles':articles})
+
+def article_detail(request, slug):
+	return HttpResponse(slug)
