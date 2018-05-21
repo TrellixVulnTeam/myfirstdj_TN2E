a71515447e553ffcc9aa88a25966f7351cde5ddc
= <=>
2018-05-21 23:05:58 +0200

    image upload

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 import articles.views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 admin.autodiscover()
 import hello.views
 # Examples:
@@ -16,3 +18,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
