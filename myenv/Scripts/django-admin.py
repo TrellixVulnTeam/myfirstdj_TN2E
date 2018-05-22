f2a15a66523ef5b77295cc0c2f936c3df5d42fe2
Fra <sworld.design@gmail.com>
2018-05-22 09:00:28 +0200

    pic

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\informatica\desktop\stark-woodland-27830\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
