0e0a8877ce5b1cbc1056f13be59c584a51557b05
= <=>
2018-05-27 18:59:20 +0200

    author

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 # Create your models here.
 class Article(models.Model):
 	"""docstring for Article"""
@@ -6,7 +7,9 @@ class Article(models.Model):
 	slug = models.SlugField()
 	body = models.TextField()
 	date = models.DateTimeField(auto_now_add = True)
-	tumb = models.ImageField(default = 'default.png', blank = True)	
+	tumb = models.ImageField(default = 'default.png', blank = True)
+	author = models.ForeignKey(User, default=None, on_delete = models.CASCADE)
+	
 	def __str__(self):
 		return self.title
 
@@ -14,4 +17,4 @@ class Article(models.Model):
 		if len(self.body) > 100:
 			return self.body[:100] + "..."
 		else:
-			return self.body
+			return self.bod
